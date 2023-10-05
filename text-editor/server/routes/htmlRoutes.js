b6f931ccd596343aef944b249e9e070f826185b0
Ismail <63372291+elixit@users.noreply.github.com>
2023-10-04 22:57:41 -0700

    Delete text-editor directory

--- text-editor/server/routes/htmlRoutes.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-// using a client/dist folder, so the bundles need to be under it
-module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-  );
