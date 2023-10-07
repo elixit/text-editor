cfe1b06d247704f9fced6ca5a23a6bfe138c4525
Ismail <63372291+elixit@users.noreply.github.com>
2023-10-06 22:39:44 -0700

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,10 +14,10 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
-    const jateDb = await openDB('jate', 1);
+    const db = await openDB('jate', 1);
     const request = store.put({ id: 1, value: content });
     const result = store.put({ id: 1, value: content });
-    const tx = jateDb.transaction('jate', 'readwrite');
+    const tx = db.transaction('jate', 'readwrite');
     const store = tx.objectStore('jate');
     console.log(request, result);
   
@@ -26,12 +26,16 @@ export const putDb = async (content) => console.error('putDb not implemented');
   //TODO: Add logic for a method that gets all the content from the database
   export const getDb = async () => console.log('Get from database');
   {
+  
+
+    const id = await store.put({ value: content, id:1 })
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');  
     const store = tx.objectStore('jate');
-    const jateDb = await openDB('jate', 1);
-    const tx = jateDb.transaction('jate', 'readonly');
-    const request = store.getAll();
-    const result = await request;
-    return result?.value;
+    const content = await store.get(1);
+    await tx.done;
+  
+    return content?.value;
   };
   
   
